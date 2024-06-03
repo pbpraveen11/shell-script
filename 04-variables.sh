aa51a2e6477aec336fac9b5e0aa89aa674eff3d2
pbpraveen11 <pbpraveenkumar11@gmail.com>
2024-06-03 11:54:30 +0530

    helo script

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#declearing of variable
+PERSON1=$1
+PERSON2=$2
+
+#referring of variable
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I'm fine, How are you ?"
+echo "$PERSON1:: I'm fine too, how is your work"
+echo "$PERSON2:: not bad, I'm thinking to upgrade to DevOps"

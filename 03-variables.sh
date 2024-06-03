0a003777e41678ff1e4b624be22a0ea9d057a100
pbpraveen11 <pbpraveenkumar11@gmail.com>
2024-06-03 11:47:09 +0530

    helo script

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,6 +1,10 @@
-PERSON1=Ramesh
-PERSON2=Suresh
 
+
+#declearing of variable
+PERSON1=Sachin
+PERSON2=Rahul
+
+#referring of variable
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I'm fine, How are you ?"
 echo "$PERSON1:: I'm fine too, how is your work"

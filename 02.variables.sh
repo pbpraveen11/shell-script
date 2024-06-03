3b96aac059a99819cc67e25953ce0f86eda672b5
pbpraveen11 <pbpraveenkumar11@gmail.com>
2024-06-03 08:11:09 +0530

    helo script

---
+++ 02.variables.sh
@@ -0,0 +1,4 @@
+echo "Ramesh:: Hello Suresh, How are you?"
+echo "Suresh:: Hi Ramesh, I'm fine, How are you ?"
+echo "Ramesh:: I'm fine too, how is your work"
+echo "Suresh:: not bad, I'm thinking to upgrade to DevOps"

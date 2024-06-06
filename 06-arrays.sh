ef4ffe1adef1d5e1d769216ae2903673af145734
pbpraveen11 <pbpraveenkumar11@gmail.com>
2024-06-06 11:06:07 +0530

    helo script

---
+++ 06-arrays.sh
@@ -0,0 +1,11 @@
+Movies=("RRR" "DJ Tillu" "Manam" "VakeelSaab")
+
+#size of above array is 4
+#Index are 0,1,2,3
+
+#list always starts with 0
+
+echo "First movie name is: ${Movie[0]}"
+echo "First movie name is: ${Movie[1]}"
+echo "First movie name is: ${Movie[2]}"
+echo "First movie name is: ${Movie[@]}"

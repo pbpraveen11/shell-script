65e1d07c678822c695277bc1f7bc1da38dd50f17
pbpraveen11 <pbpraveenkumar11@gmail.com>
2024-06-06 10:46:59 +0530

    helo script

---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+echo "Please enter username::"
+
+read -s USERNAME #here USERNAME is variable
+
+echo "Plese enter Password::"
+
+read -s PASSWORD 
+
+echo "username is: $USERNAME, Password is: $PASSWORD"

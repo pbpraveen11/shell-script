88a1b3e30ed5f14e65df9b34c2148b6e6f21665c
pbpraveen11 <pbpraveenkumar11@gmail.com>
2024-06-07 11:23:34 +0530

    helo script

--- 08-specialvariables.sh
+++ 08-specialvariables.sh
@@ -5,5 +5,6 @@ echo "No of variables passed: $#"
 echo "Script name: $0"
 echo "Current Working Directory: $PWD"
 echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
 
 

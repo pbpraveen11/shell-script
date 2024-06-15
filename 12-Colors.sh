2666b1a210994989a3e12fb8b1a3a17669595026
pbpraveen11 <pbpraveenkumar11@gmail.com>
2024-06-15 13:18:20 +0530

    Colors Script

--- 12-Colors.sh
+++ 12-Colors.sh
@@ -2,8 +2,11 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$($0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+echo "Script started executing at: $TIMESTAMP"
+
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"

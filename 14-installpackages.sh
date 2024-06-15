25df88831cd3fc8f6abead522e630f1ab9b89bfa
pbpraveen11 <pbpraveenkumar11@gmail.com>
2024-06-15 15:39:03 +0530

    Colors Script

--- 14-installpackages.sh
+++ 14-installpackages.sh
@@ -4,10 +4,21 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi    
+}
 
 if [ $USERID -ne 0 ]
 then
@@ -23,7 +34,7 @@ do
     dnf list installed $i &>>$LOGFILE
     if [ $i -eq 0 ]
     then
-        echo "$i Packed already installed ...$Y SKIPPING $N"
+        echo -e "$i Packed already installed ...$Y SKIPPING $N"
     else
         echo "$i not installed...Need to Install"
     fi 

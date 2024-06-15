f77a0dd00620300dd67f49ab44eee7c370bac963
pbpraveen11 <pbpraveenkumar11@gmail.com>
2024-06-15 15:33:42 +0530

    Colors Script

--- 14-installpackages.sh
+++ 14-installpackages.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with root acces."
@@ -12,4 +20,11 @@ fi
 for i in $@
 do
     echo "Package to install: $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $i -eq 0 ]
+    then
+        echo -e "$i Packed already installed ...$Y SKIPPING $N"
+    else
+        echo "$i not installed...Need to Install"
+    fi 
 done

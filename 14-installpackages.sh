0ba82a97b736264eb10e21bb12aff781b327cbb5
pbpraveen11 <pbpraveenkumar11@gmail.com>
2024-06-15 15:16:00 +0530

    Colors Script

--- 14-installpackages.sh
+++ 14-installpackages.sh
@@ -5,6 +5,8 @@ if [ $USERID -ne 0]
 then
     echo "Please run this script with root acces."
     exit 1 # manually exit if error comes
+else
+    echo "You are a super user"    
 fi
 
 echo "All Packages: $@"
